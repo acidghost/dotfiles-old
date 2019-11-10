e8c1a89cb54958ae5cdc5d11821ba99969427cfb
Andrea Jemmett <andreajemmett@gmail.com>
2019-11-10 11:30:24 +0100

    [deps.sh] minor fix

--- get_dotfiles_deps.sh
+++ get_dotfiles_deps.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 
+set -e
+
 echo 'Installing some apt packages...'
 sudo apt install curl python3-pip python-pygments
 
-if test -e ~/antigen.zsh; then
+# Install Antigen (zsh plugin manager)
+if [[ -e ~/antigen.zsh ]]; then
     echo 'Antigen is already installed'
 else
     echo 'Installing Antigen...'
     curl -L git.io/antigen > ~/antigen.zsh
 fi
 
-if test -e ~/.vim/autoload/plug.vim; then
+# Install Plug (Vim plugin manager)
+if [[ -e ~/.vim/autoload/plug.vim ]]; then
     echo 'Plug is already installed'
 else
     echo 'Installing Plug...'
@@ -18,4 +22,8 @@ else
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
-pip3 install python-language-server 'python-language-server[yapf]' 'python-language-server[pyflakes]'
+# Install Python language server + YAPF & PyFlakes
+pip3 install python-language-server \
+    'python-language-server[yapf]' \
+    'python-language-server[pyflakes]'
+
