9a76405914ea16f7507463142fd7a0d60d86700c
Andrea Jemmett <andreajemmett@gmail.com>
2019-11-10 17:47:16 +0100

    [xmonad] added volume keys & reformat

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -8,22 +8,27 @@ import           Graphics.X11.ExtraTypes.XF86
 
 main = do
     xmproc <- spawnPipe "xmobar"
-    xmonad
-        $                 def
-                              { modMask = mod4Mask
-                              , terminal = "urxvt"
-                              , borderWidth = 1
-                              , normalBorderColor = "#dddddd"
-                              , focusedBorderColor = "#00ff00"
-                              , manageHook = manageDocks <+> manageHook defaultConfig
-                              , layoutHook = avoidStruts $ layoutHook defaultConfig
-                              , handleEventHook = handleEventHook defaultConfig
-                                                      <+> docksEventHook
-                              , logHook = dynamicLogWithPP xmobarPP
-                                              { ppOutput = hPutStrLn xmproc
-                                              , ppTitle = xmobarColor "green" "" . shorten 50
-                                              }
-                              }
-        `additionalKeysP` [ ("<XF86MonBrightnessUp>"  , spawn "lux -a 10%")
-                          , ("<XF86MonBrightnessDown>", spawn "lux -s 10%")
-                          ]
+    xmonad $ (myConfig xmproc) `additionalKeysP` myKeysXF86
+
+myConfig xmproc = def
+    { modMask            = mod4Mask
+    , terminal           = "urxvt"
+    , borderWidth        = 1
+    , normalBorderColor  = "#dddddd"
+    , focusedBorderColor = "#00ff00"
+    , manageHook         = manageDocks <+> manageHook defaultConfig
+    , layoutHook         = avoidStruts $ layoutHook defaultConfig
+    , handleEventHook    = handleEventHook defaultConfig <+> docksEventHook
+    , logHook            = dynamicLogWithPP xmobarPP
+                               { ppOutput = hPutStrLn xmproc
+                               , ppTitle  = xmobarColor "green" "" . shorten 180
+                               }
+    }
+
+myKeysXF86 =
+    [ ("<XF86MonBrightnessUp>"  , spawn "lux -a 10%")
+    , ("<XF86MonBrightnessDown>", spawn "lux -s 10%")
+    , ("<XF86AudioLowerVolume>" , spawn "amixer set Master 10%-")
+    , ("<XF86AudioRaiseVolume>" , spawn "amixer set Master 10%+")
+    , ("<XF86AudioMute>"        , spawn "amixer set Master toggle")
+    ]
