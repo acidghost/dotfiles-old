db57f38513bd1529c6754385d016b5e963316f28
Andrea Jemmett <andreajemmett@gmail.com>
2019-11-15 15:24:47 +0100

    [xmobar] remove everything to avoid CPU usage

--- .xmobar/xmobar-config/app/Top.hs
+++ .xmobar/xmobar-config/app/Top.hs
@@ -16,14 +16,9 @@ main = config >>= xmobar
   where
     config = configFromArgs $ baseConfig
         { commands = [ Run StdinReader
-                     , Run cpu
-                     , Run memory
-                     , Run swap
                      , Run date
-                     , Run batt
                      ]
         , template =
             "|StdinReader| }{ \
-            \|battery| |cpu| |memory| * |swap| \
             \<fc=#00FF00>|date|</fc>"
         }
