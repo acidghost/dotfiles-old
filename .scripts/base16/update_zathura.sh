e889460d6b78712e8ce9534e9f928c3e88b5f67e
Andrea Jemmett <1787979+acidghost@users.noreply.github.com>
2021-08-18 12:43:20 +0200

    [base16] hook to update zathura theme

---
+++ .scripts/base16/update_zathura.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+ln -sf "$HOME/.antigen/bundles/HaoZeke/base16-zathura-main/build_schemes/colors/base16-$BASE16_THEME.config" \
+       "$HOME/.config/zathura/base16-theme.config"
